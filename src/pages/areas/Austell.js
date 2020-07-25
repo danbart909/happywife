ad3af0315d2901f7ca7700cc6eaf3decd6259099
Dan Bartlett <danbart909@gmail.com>
2020-07-24 22:11:48 -0400

    areas

--- src/pages/areas/Austell.js
+++ src/pages/areas/Austell.js
@@ -10,7 +10,14 @@ export default class Austell extends Component {
           <meta name="description" content="Happy Wife - Austell" />
         </Helmet>
         <div className='areas Austell UL'>
-          <span>Our Austell Junk Removal team believes that you deserve to know that everyone who comes into your house should be upright, honest men and women. That’s why we honor your home and your family. We will never track dirt into you house or use profanity around you or your children. Your comfort and ease of mind is our utmost concern. We never provide a service that we wouldn’t be proud of! Whether you need furniture removal, appliance removal, property clean out, or and other form of household junk, Happy Wife junk service is there for you. Our fast, reliable service is a favorite for those looking for Acworth junk hauling.</span>
+          <div className='services-box'>
+            <div className='services-title'>
+              <span>Austell</span>
+            </div>
+            <div className='services-body'>
+              <span>Our Austell Junk Removal team believes that you deserve to know that everyone who comes into your house should be upright, honest men and women. That’s why we honor your home and your family. We will never track dirt into you house or use profanity around you or your children. Your comfort and ease of mind is our utmost concern. We never provide a service that we wouldn’t be proud of! Whether you need furniture removal, appliance removal, property clean out, or and other form of household junk, Happy Wife junk service is there for you. Our fast, reliable service is a favorite for those looking for Acworth junk hauling.</span>
+            </div>
+          </div>
         </div>
       </>
     )
--- src/pages/areas/PowderSprings.js
+++ src/pages/areas/PowderSprings.js
@@ -10,7 +10,14 @@ export default class PowderSprings extends Component {
           <meta name="description" content="Happy Wife - PowderSprings" />
         </Helmet>
         <div className='areas PowderSprings UL'>
-          <span>Our Powder Springs Junk Removal team believes that you deserve to know that everyone who comes into your house should be upright, honest men and women. That’s why we honor your home and your family. We will never track dirt into you house or use profanity around you or your children. Your comfort and ease of mind is our utmost concern. We never provide a service that we wouldn’t be proud of! Whether you need furniture removal, appliance removal, property clean out, or and other form of household junk, Happy Wife junk service is there for you. Our fast, reliable service is a favorite for those looking for Acworth junk hauling.</span>
+          <div className='services-box'>
+            <div className='services-title'>
+              <span>Powder Springs</span>
+            </div>
+            <div className='services-body'>
+              <span>Our Powder Springs Junk Removal team believes that you deserve to know that everyone who comes into your house should be upright, honest men and women. That’s why we honor your home and your family. We will never track dirt into you house or use profanity around you or your children. Your comfort and ease of mind is our utmost concern. We never provide a service that we wouldn’t be proud of! Whether you need furniture removal, appliance removal, property clean out, or and other form of household junk, Happy Wife junk service is there for you. Our fast, reliable service is a favorite for those looking for Acworth junk hauling.</span>
+            </div>
+          </div>
         </div>
       </>
     )
