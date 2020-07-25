723e50f2248e98c495068bf4cca73bdc0579dac4
Dan Bartlett <danbart909@gmail.com>
2020-07-24 21:33:09 -0400

    added services

--- src/pages/services/GarageCleanOut.js
+++ src/pages/services/GarageCleanOut.js
@@ -10,7 +10,14 @@ export default class GarageCleanOut extends Component {
           <meta name="description" content="Happy Wife - Garage Clean Out" />
         </Helmet>
         <div className='services GarageCleanOut UL'>
-          <span>Garage clean out is a breeze with Happy Wife Junk Service. Our costumer driven junk service wants your experience to be pleasant and stress free. We haul any kinds of junk you need. Happy Wife shows up on time and is a breeze to work with. We always put the needs of our clients first. We are determined to ensure that you feel comfortable and safe inviting us into your home. We always strive to serve you and provide you with a valuable service. Send us a text and we will contact you proptly, and your junk will be gone before you knew it possible.</span>
+          <div className='services-box'>
+            <div className='services-title'>
+              <span>Garage Clean Out</span>
+            </div>
+            <div className='services-body'>
+              <span>Garage clean out is a breeze with Happy Wife Junk Service. Our costumer driven junk service wants your experience to be pleasant and stress free. We haul any kinds of junk you need. Happy Wife shows up on time and is a breeze to work with. We always put the needs of our clients first. We are determined to ensure that you feel comfortable and safe inviting us into your home. We always strive to serve you and provide you with a valuable service. Send us a text and we will contact you proptly, and your junk will be gone before you knew it possible.</span>
+            </div>
+          </div>
         </div>
       </>
     )
