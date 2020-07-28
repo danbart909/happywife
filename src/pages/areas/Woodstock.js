28ee3b6352ec9ed2fe3b322a5f822fcc53d5a106
Dan Bartlett <danbart909@gmail.com>
2020-07-27 22:41:40 -0400

    typos fixed

--- src/pages/areas/Woodstock.js
+++ src/pages/areas/Woodstock.js
@@ -15,7 +15,7 @@ export default class Woodstock extends Component {
               <span>Woodstock</span>
             </div>
             <div className='services-body'>
-              <span>Our Woodstock Junk Removal team believes that you deserve to know that everyone who comes into your house should be upright, honest men and women. That’s why we honor your home and your family. We will never track dirt into you house or use profanity around you or your children. Your comfort and ease of mind is our utmost concern. We never provide a service that we wouldn’t be proud of! Whether you need furniture removal, appliance removal, property clean out, or and other form of household junk, Happy Wife junk service is there for you. Our fast, reliable service is a favorite for those looking for Acworth junk hauling.</span>
+              <span>Our Woodstock Junk Removal team believes that you deserve to know that everyone who comes into your house should be upright, honest men and women. That’s why we honor your home and your family. We will never track dirt into you house or use profanity around you or your children. Your comfort and ease of mind is our utmost concern. We never provide a service that we wouldn’t be proud of! Whether you need furniture removal, appliance removal, property clean out, or and other form of household junk, Happy Wife junk service is there for you. Our fast, reliable service is a favorite for those looking for Woodstock junk hauling.</span>
             </div>
           </div>
         </div>
--- src/pages/services/LoveSeatRemoval.js
+++ src/pages/services/LoveSeatRemoval.js
@@ -15,7 +15,7 @@ export default class LoveSeatRemoval extends Component {
               <span>Love Seat Removal</span>
             </div>
             <div className='services-body'>
-              <span>Love seat removal is a breeze with Happy Wife Junk Service. Our costumer driven junk service wants your experience to be pleasant and stress free. We haul any kinds of junk you need. Happy Wife shows up on time and is a breeze to work with. We always put the needs of our clients first. We are determined to ensure that you feel comfortable and safe inviting us into your home. We always strive to serve you and provide you with a valuable service. Send us a text and we will contact you proptly, and your junk will be gone before you knew it possible.</span>
+              <span>Love seat removal is a breeze with Happy Wife Junk Service. Our customer driven junk service wants your experience to be pleasant and stress free. We haul any kind of junk you need. Happy Wife shows up on time and is a breeze to work with. We always put the needs of our clients first. We are determined to ensure that you feel comfortable and safe inviting us into your home. We always strive to serve you and provide you with a valuable service. Send us a text and we will contact you proptly, and your junk will be gone before you knew it possible.</span>
             </div>
           </div>
         </div>
