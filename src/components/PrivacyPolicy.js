e50391e730af1a298371957589625f201ea7f242
Dan Bartlett <danbart909@gmail.com>
2022-02-28 18:17:55 -0500

    mowr privacy policy update 2

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -8,14 +8,14 @@ export default class PrivacyPolicy extends Component {
 
           <div id='aboutus' className='aboutus-box'>
             <div id='aboutus-1' className='aboutus-title'>
-              <span>About Us</span>
+              <span>Privacy Policy for mowr</span>
             </div>
             <div className='aboutus-hr'>
               <hr />
             </div>
             <div id='aboutus-2' className='aboutus-text'>
-              <span id='about-us-2-span' className='aboutus-text-span'>All information is kept to the Google Firebase system and none of it is used to identify anybody nor is it sold for profit.</span>
-              <span>Pretty Good Inc. © 2022</span>
+              <span className='aboutus-text'>All information is kept to the Google Firebase system and none of it is used to identify anybody nor is it sold for profit.</span>
+              <span className='aboutus-text2'>Pretty Good Inc. © 2022</span>
             </div>
           </div>
 
