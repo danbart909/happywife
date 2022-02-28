02e622d1201a322bc88a40e8305763151dbb3d7c
Dan Bartlett <danbart909@gmail.com>
2022-02-28 18:06:02 -0500

    bit change

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -6,30 +6,18 @@ export default class PrivacyPolicy extends Component {
       <>
        <div id='aboutus-container'>
 
-        <div id='aboutus' className='aboutus-box'>
-          <div id='aboutus-1' className='aboutus-title'>
-            <span>About Us</span>
+          <div id='aboutus' className='aboutus-box'>
+            <div id='aboutus-1' className='aboutus-title'>
+              <span>About Us</span>
+            </div>
+            <div className='aboutus-hr'>
+              <hr />
+            </div>
+            <div id='aboutus-2' className='aboutus-text'>
+              <span id='about-us-2-span' className='aboutus-text-span'>All information is kept to the Google Firebase system and none of it is used to identify anybody nor is it sold for profit.</span>
+              <span>Pretty Good Inc. © 2022</span>
+            </div>
           </div>
-          <div className='aboutus-hr'>
-            <hr />
-          </div>
-          <div id='aboutus-2' className='aboutus-text'>
-            <span id='about-us-2-span' className='aboutus-text-span'>All information is kept to the Google Firebase system and none of it is used to identify anybody nor is it sold for profit.</span>
-          </div>
-        </div>
-
-        <div id='whatwebelieve' className='aboutus-box'>
-          <div id='whatwebelieve-1' className='aboutus-title'>
-            <span>Information</span>
-          </div>
-          <div className='aboutus-hr'>
-            <hr />
-          </div>
-          <div id='whatwebelieve-2' className='aboutus-text'>
-            <span>Nothing happens to your information! Yay!</span>
-            <span>Pretty Good Inc. © 2022</span>
-          </div>
-        </div>
 
         </div> 
       </>
