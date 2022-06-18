2275fd8eae6544ee7d00aaf0a73e39c63f34f7f6
Dan Bartlett <danbart909@gmail.com>
2022-06-18 19:57:13 -0400

    .

--- src/components/MidBanner.js
+++ src/components/MidBanner.js
@@ -10,7 +10,7 @@ export default class MidBanner extends Component {
               <span>Call Now</span>
             </div>
             <div id='midbanner-number'>
-              <a href='tel:470-304-7632'><span>470.304.7632</span></a>
+              <a href='tel:762-217-3303'><span>762.217.3303</span></a>
             </div>
           </div>
         </div>
