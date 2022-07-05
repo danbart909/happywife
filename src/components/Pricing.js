790ba6a4e82eb77e536c62e04dd15e5baecee976
Dan Bartlett <danbart909@gmail.com>
2022-07-05 07:59:24 -0400

    pricing update

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -18,7 +18,7 @@ export default class Pricing extends Component {
               <span>A full load is almost always large enough to clean out the excess junk from a 5 person, 3200 sq ft house!</span>
               <span>We only charge you for what you use!</span>
               <span>A full load is $475</span>
-              <span>A 2/3 Load is $350</span>
+              <span>A 2/3 load is $350</span>
               <span>A 1/3 load is $250</span>
               <span>For single items or a small amount, call or email us for a quote!</span>
             </div>
